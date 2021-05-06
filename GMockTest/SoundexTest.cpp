381589cac440f13ed056877825313d06e4c9dc16
Frederique Hsu <frederique.hsu@outlook.com>
2021-05-07 02:24:03 +0800

    编写更多用例来推动对Soundex的进一步开发，使Soundex类提供更多Feature，更健壮.

--- GMockTest/SoundexTest.cpp
+++ GMockTest/SoundexTest.cpp
@@ -25,4 +25,45 @@ TEST_F(SoundexEncoding, ReplacesConstantsWithAppropriateDigits)
 {
     EXPECT_THAT(soundex.encode("Ab"), Eq("A100"));
     EXPECT_THAT(soundex.encode("Ac"), Eq("A200"));
+    EXPECT_THAT(soundex.encode("Ad"), Eq("A300"));
+    EXPECT_THAT(soundex.encode("Ax"), Eq("A200"));
+}
+
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics)
+{
+    ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits)
+{
+    ASSERT_THAT(soundex.encode("Acdl"), Eq("A234"));
+}
+
+TEST_F(SoundexEncoding, LimitsLengthToFourCharacters)
+{
+    ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u));
+}
+
+TEST_F(SoundexEncoding, IgnoresVowelLikeLetters)
+{
+    ASSERT_THAT(soundex.encode("BaAeEiIoOuUHyYcdl"), Eq("B234"));
+}
+
+TEST_F(SoundexEncoding, CombinesDuplicateEncodings)
+{
+    ASSERT_THAT(soundex.encodedDigit('b'), Eq(soundex.encodedDigit('f')));
+    ASSERT_THAT(soundex.encodedDigit('c'), Eq(soundex.encodedDigit('g')));
+    ASSERT_THAT(soundex.encodedDigit('d'), Eq(soundex.encodedDigit('t')));
+
+    ASSERT_THAT(soundex.encode("Abfcgdt"), Eq("A123"));
+}
+
+TEST_F(SoundexEncoding, UppercasesFirstLetter)
+{
+    ASSERT_THAT(soundex.encode("abcd"), StartsWith("A"));
+}
+
+TEST_F(SoundexEncoding, IgnoresCaseWhenEncodingConsonants)
+{
+    ASSERT_THAT(soundex.encode("BCDL"), Eq(soundex.encode("Bcdl")));
 }
