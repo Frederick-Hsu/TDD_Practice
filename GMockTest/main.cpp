29d33b738cc89ce1b562cf11c09482295f4d06a5
Frederique Hsu <frederique.hsu@outlook.com>
2021-05-07 02:22:14 +0800

    若使用GoogleTest作单元测试，你的C++项目必须支持C++11以上标准。

--- GMockTest/main.cpp
+++ GMockTest/main.cpp
@@ -1,9 +1,12 @@
-#include "tst_gmocktestcase.h"
+// #include "tst_gmocktestcase.h"
 
 #include <gtest/gtest.h>
 
 int main(int argc, char *argv[])
 {
+    std::cout << "Current C++ Standard No. = " << __cplusplus << std::endl;
+    std::cout << "Current program is " << sizeof(void*) * 8 << "-bit\n" << std::endl;
+
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
